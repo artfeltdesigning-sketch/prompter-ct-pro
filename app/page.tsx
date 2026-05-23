7078bb24ea35adf317a67eefebd09e5279a12e7f
artfeltdesigning-sketch <artfeltdesigning@gmail.com>
2026-05-23 19:23:08 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Sidebar from "@/components/Sidebar";
-import PromptWorkspace from "@/components/PromptWorkspace";
-import ResultPanel from "@/components/ResultPanel";
+import Sidebar from "../components/Sidebar";
+import PromptWorkspace from "../components/PromptWorkspace";
+import ResultPanel from "../components/ResultPanel";
 
 export default function Home() {
   return (
-    <main className="studio-shell">
+    <div>
       <Sidebar />
       <PromptWorkspace />
       <ResultPanel />
-    </main>
+    </div>
   );
 }
